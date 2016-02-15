fcec9207286d130ea95174b09e43386df8b5a37f
Joshua Slate <josh.a.slate@gmail.com>
2016-02-15 08:38:17 -0600

    Updating server.js
    Just making changes I figured would make part 2 of the tutorial (authentication) easier.

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 // Include our packages in our main server file
 var express = require('express');
-var mongoose = require('mongoose');
-var path = require('path');
-var port = 3000;
-
 app = express();
+var mongoose = require('mongoose');
+var port = 3000;
 
 app.listen(port);
 console.log('Your server is running on port ' + port + '.');
