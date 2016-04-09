4f2694010f72d6169e9fbce676612d76d115d81f
Joshua Slate <josh.a.slate@gmail.com>
2016-04-08 21:49:12 -0500

    Updating MongoDB connection.

--- config/main.js
+++ config/main.js
@@ -1,4 +1,4 @@
 module.exports = {
   'secret': 'longobnoxiouspassphrase',
-  'database': 'mongodb://127.0.0.1:27017/saas-tutorial'
+  'database': 'mongodb://localhost:saas-tutorial/saas-tutorial'
 };
