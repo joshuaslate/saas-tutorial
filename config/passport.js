d5f3acc25bb14b4c543bf199fc4ce47cde00b764
Joshua Slate <josh.a.slate@gmail.com>
2016-04-08 22:11:04 -0500

    Fixing typos on object notation.

--- config/passport.js
+++ config/passport.js
@@ -6,8 +6,8 @@ const config = require('../config/main');
 // Setup work and export for the JWT passport strategy
 module.exports = function(passport) {
   const opts = {
-    jwtFromRequest = ExtractJwt.fromAuthHeader();
-    secretOrKey = config.secret;
+    jwtFromRequest: ExtractJwt.fromAuthHeader(),
+    secretOrKey: config.secret
   };
   passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
     User.findOne({id: jwt_payload.id}, function(err, user) {
